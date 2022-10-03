97b28dc674a98d6fa8192bdc1db16ec4026b533a
Charlie McCain <cmccain@intellum.com>
2022-10-03 02:42:51 -0400

    fix typo

--- create-release.js
+++ create-release.js
@@ -4,6 +4,7 @@ module.exports = async ({github, context}) => {
     repo: context.repo.repo
   })
   let tag_name = resp.data.tag_name
+
   // sha of latest release tag
   resp = await github.rest.git.listMatchingRefs({
     owner: context.repo.owner,
@@ -11,12 +12,14 @@ module.exports = async ({github, context}) => {
     ref: `tags/${tag_name}`
   })
   let tag_sha = resp.data[0].object.sha
+
   resp = await github.rest.git.getCommit({
     owner: context.repo.owner,
     repo: context.repo.repo,
     commit_sha: tag_sha
   })
   let release_commit = resp.data
+
   // commits since latest release
   resp = await github.rest.repos.listCommits({
     owner: context.repo.owner,
@@ -24,15 +27,18 @@ module.exports = async ({github, context}) => {
     since: release_commit.author.date
   })
   let commits = resp.data
+
   commits.pop()
   let issues = []
   let commits_without_issues = []
-  const regexp = /(intellum\/[\w-]+)?\(?#\d+\)?/g;
-  let owner_repo = '${{ github.repository }}'
-  let repo = owner_repo.split('/')[1]
+
   commits.forEach(commit => {
     let message = commit.commit.message
+    let owner_repo = '${{ github.repository }}'
+    let repo = owner_repo.split('/')[1]
+    const regexp = /(intellum\/[\w-]+)?\(?#\d+\)?/g
     const issue_refs = [...message.matchAll(regexp)].map(e => e[0])
+
     if (issue_refs.length == 0) {
       commits_without_issues.push({
         repo: repo,
@@ -41,10 +47,12 @@ module.exports = async ({github, context}) => {
       })
       return
     }
+
     issue_refs.forEach(issue_ref => {
       if (issue_ref.startsWith('(')) {
         return
       }
+
       if (issue_ref.startsWith('intellum')) {
         let [owner_repo, issue_number] = issue_ref.split('#')
         issues.push({
@@ -59,6 +67,7 @@ module.exports = async ({github, context}) => {
       }
     })
   })
+
   return {
     issues: issues,
     commits_without_issues: commits_without_issues
