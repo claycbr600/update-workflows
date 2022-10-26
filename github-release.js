6e706e4e46b320d12b1f20192d1476e7e9c9f372
Charlie McCain <cmccain@intellum.com>
2022-10-26 15:16:08 -0400

    Add github release script (#4)

--- github-release.js
+++ github-release.js
@@ -1,33 +1,57 @@
 module.exports = async ({ github, context }) => {
-  let resp = await github.rest.repos.getLatestRelease({
-    owner: context.repo.owner,
-    repo: context.repo.repo
-  })
-  let tag_name = resp.data.tag_name
+  let resp, tag_name, commits
 
-  // commit sha of latest release tag
-  resp = await github.rest.git.listMatchingRefs({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `tags/${tag_name}`
-  })
-  let tag_commit_sha = resp.data[0].object.sha
+  // get latest release
+  try {
+    resp = await github.rest.repos.getLatestRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo
+    })
+    tag_name = resp.data.tag_name
+  } catch (error) {
+    if (error.response.status == 404) {
+      console.log("No previous release found")
+    } else {
+      throw error
+    }
+  }
 
-  resp = await github.rest.git.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    commit_sha: tag_commit_sha
-  })
-  let release_commit = resp.data
+  // get commits since last release
+  if (tag_name) {
+    // commit sha of latest release tag
+    resp = await github.rest.git.listMatchingRefs({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: `tags/${tag_name}`
+    })
+    let tag_commit_sha = resp.data[0].object.sha
 
-  // commits since latest release
-  resp = await github.rest.repos.listCommits({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    since: release_commit.author.date
-  })
-  let commits = resp.data
-  commits.pop()
+    resp = await github.rest.git.getCommit({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: tag_commit_sha
+    })
+    let release_commit = resp.data
+
+    // commits since latest release
+    resp = await github.rest.repos.listCommits({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      since: release_commit.author.date,
+      per_page: 100
+    })
+    commits = resp.data
+    commits.pop()
+
+  // get all commits
+  } else {
+    resp = await github.rest.repos.listCommits({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      per_page: 100
+    })
+    commits = resp.data
+  }
 
   let issues = [], commits_without_issues = []
 
@@ -39,10 +63,9 @@ module.exports = async ({ github, context }) => {
 
     if (issue_refs.length == 0) {
       let title = message.split('\n')[0]
-      commits_without_issues.push({
-        title: title,
-        link: `* [${title}](https://github.com/intellum/${repo}/commit/${commit.sha})`
-      })
+      commits_without_issues.push(
+        `* [${title}](https://github.com/intellum/${repo}/commit/${commit.sha})`
+      )
       continue
     }
 
@@ -65,25 +88,28 @@ module.exports = async ({ github, context }) => {
       })
       let issue = resp.data
 
-      issues.push({
-        title: issue.title,
-        link: `* [${issue.title}](https://github.com/intellum/${repo}/issues/${issue_number})`
-      })
+      issues.push(
+        `* [${issue.title}](https://github.com/intellum/${repo}/issues/${issue_number})`
+      )
     }
   }
+
+  // uniq lists
+  issues = [...new Set(issues)]
+  commits_without_issues = [...new Set(commits_without_issues)]
 
   let output = []
   if (issues.length > 0) {
     output = output.concat([
       "\nIssues",
-      ...issues.map(issue => issue.link)
+      ...issues
     ])
   }
 
   if (commits_without_issues.length > 0) {
     output = output.concat([
       "\nCommits without an issue number",
-      ...commits_without_issues.map(cwi => cwi.link)
+      ...commits_without_issues
     ])
   }
 
